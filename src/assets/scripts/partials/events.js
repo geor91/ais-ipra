f0725830ac30ce913c6a61920b853849a7410854
Johny Martin <jorjiko91@gmail.com>
2016-04-11 12:22:18 +0300

    v 1.0.1 - ajax pages, ie8 some fixes

--- src/assets/scripts/partials/events.js
+++ src/assets/scripts/partials/events.js
@@ -1,7 +1,27 @@
-$('.page__backward').on('click', function() {
-	$('.main-content').removeClass('main-content_scrolled');
-});
+$('body').delegate('.js-ajax-link', 'click', function() {
+	var targetId        = $(this).data('load') || null,
+	    targetPage      = '',
+	    targetPageHtml  = '',
+	    currPageId      = $(this).parents('.page').attr('id'),
+	    $pagesContainer = $('.main-content');
 
-$('.table-wrap tr').on('click', function() {
-	$('.main-content').addClass('main-content_scrolled');
+	if (targetId) {
+		targetId        = '#' + targetId;
+		currPageId      = '#' + currPageId;
+		targetPageHtml  = $(targetId + 'tmp').html();
+
+		if (targetPageHtml) {
+			$pagesContainer.append(targetPageHtml);
+			$(currPageId).attr('data-child', targetId);
+			$(targetId).attr('data-parent', currPageId);
+			$(currPageId).addClass('page_hidden');
+
+			$(targetId + 'tmp').remove();
+		}
+	}
+	else {
+		targetId = $(this).parents('.page').data('parent');
+		$(targetId).removeClass('page_hidden');
+		$(currPageId).remove();
+	}
 });
